ec74322e32cb7a47ef9ccf5f2645774da782878b
rhit-mcneill1 <mcneill1@rose-hulman.edu>
2020-10-27 16:25:26 -0400

    made updating_pc use reg

--- implementation/processor-2v/integration_updating_pc.v
+++ implementation/processor-2v/integration_updating_pc.v
@@ -1,26 +1,30 @@
 `timescale 1ns / 1ps
 
 module integration_updating_pc(
-    input PCWrite,
-    input [2:0] PCControl,
-    input [1:0] RStackOP,
-	 input CLK,
-	 input Reset,
-	 output Overflow,
-	 output wire [15:0] PC_out
-    );
+   input PCWrite,
+   input [2:0] PCControl,
+   input [1:0] RStackOP,
+	input CLK,
+	input Reset,
+	output Overflow,
+	output wire [15:0] PC_out
+   );
 	 
-	 wire [15:0] Adder_out;
-	 wire [15:0] Mux_out;
-	 wire [15:0] RStack_out;
-	 
-	 register16 PC (
-		.D(Mux_out),
-		.CE(PCWrite),
-		.C(CLK),
-		.R(Reset),
-		.Q(PC_out)
-	 );
+	wire [15:0] Adder_out;
+	wire [15:0] Mux_out;
+	wire [15:0] RStack_out;
+
+	reg [15:0] PC;
+
+	assign PC_out = PC;
+	
+	always @ (posedge CLK) begin
+		if (Reset == 1)
+			PC = 0;
+		else
+			if (PCWrite == 1)
+				PC = Mux_out;
+	end
 	 
 	 mux3 mux (
 		.i0(RStack_out),
@@ -30,7 +34,7 @@ module integration_updating_pc(
 	);
 	
 	alu alu (
-		.Oper(0), 
+		.Oper(0),
 		.A(2),
 		.B(PC_out),
 		.ALU_Out(Adder_out),
